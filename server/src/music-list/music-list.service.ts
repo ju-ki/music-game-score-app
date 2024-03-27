7469969230443759b37739fb18f23f3c74a5c38a
ju-ki <juki.programming@gmail.com>
2024-03-28 07:34:30 +0900

    musicListのモデルの修正とmusicListに追加の処理

--- server/src/music-list/music-list.service.ts
+++ server/src/music-list/music-list.service.ts
@@ -12,11 +12,23 @@ export class MusicListService {
     console.log('createMusicList');
     const newMusicList = await this.prisma.musicList.create({
       data: {
+        genreId: request.genreId,
         name: request.name,
         userId: request.userId,
       },
     });
 
     return newMusicList;
+  }
+
+  async addMusicToList(request) {
+    console.log('addMusicToList');
+    await this.prisma.musicMusicList.create({
+      data: {
+        musicId: request.musicId,
+        musicListId: request.musicListId,
+        musicGenreId: request.musicGenreId,
+      },
+    });
   }
 }
