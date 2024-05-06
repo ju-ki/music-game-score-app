ad2a474870e1c08e5cce1b4bfb44073fdaa339ee
ju-ki <juki.programming@gmail.com>
2024-05-06 23:32:54 +0900

    リロード時の処理修正

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,21 +1,16 @@
 import { Controller, Get, Next, Req, Res } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Response, Request, NextFunction } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { join } from 'path';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
   @Get('/*')
   root(@Req() req: Request, @Res() res: Response, @Next() next: NextFunction): void {
     if (process.env.NODE_ENV === 'production') {
-      res.sendFile('index.html', { root: join(__dirname, '..', 'dist') });
+      res.sendFile('index.html', { root: join(__dirname, '../../', 'client/dist') });
     } else {
       next();
     }
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
     origin: ['http://localhost:5173', 'https://music-game-score-app-frontend.onrender.com'], // フロントエンドのオリジンを指定
     allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization', // 'Authorization' ヘッダーを追加
