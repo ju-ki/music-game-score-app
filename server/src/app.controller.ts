5d6e06ce62c3c64fec07f20e02b897e34bd14928
ju-ki <juki.programming@gmail.com>
2024-05-06 22:10:32 +0900

    リロード時の対応

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Next, Req, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response, Request, NextFunction } from 'express';
+import { join } from 'path';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,14 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/*')
+  root(@Req() req: Request, @Res() res: Response, @Next() next: NextFunction): void {
+    if (process.env.NODE_ENV === 'production') {
+      res.sendFile('index.html', { root: join(__dirname, '..', 'dist') });
+    } else {
+      next();
+    }
   }
 }
