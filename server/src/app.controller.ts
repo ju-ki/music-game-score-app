293a69a0076eae2e4817eb626688fb200f99a273
ju-ki <juki.programming@gmail.com>
2024-05-07 20:45:52 +0900

    ルーティングのパス修正

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -9,8 +9,9 @@ export class AppController {
 
   @Get('/*')
   root(@Req() req: Request, @Res() res: Response, @Next() next: NextFunction): void {
+    console.log(join(__dirname, '../../', 'client', 'dist', 'index.html'));
     if (process.env.NODE_ENV === 'production') {
-      res.sendFile(join(__dirname, '..', 'client', 'dist', 'index.html'));
+      res.sendFile(join(__dirname, '../../', 'client', 'dist', 'index.html'));
     } else {
       next();
     }
