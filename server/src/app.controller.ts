402b92b3342a7b3de141f1fa1c032ff992599330
ju-ki <juki.programming@gmail.com>
2024-05-07 22:28:21 +0900

    .htaccessの追加

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,19 +1,9 @@
-import { Controller, Get, Next, Req, Res } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
-import { NextFunction, Request, Response } from 'express';
-import { join } from 'path';
+// import { NextFunction, Request, Response } from 'express';
+// import { join } from 'path';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get('/*')
-  root(@Req() req: Request, @Res() res: Response, @Next() next: NextFunction): void {
-    console.log(join(__dirname, '../../', 'client', 'dist', 'index.html'));
-    if (process.env.NODE_ENV === 'production') {
-      res.sendFile(join(__dirname, '../../', 'client', 'dist', 'index.html'));
-    } else {
-      next();
-    }
-  }
 }
