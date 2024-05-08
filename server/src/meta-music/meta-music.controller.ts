a4dfeddd875a5e2788d0fe05f769a07e02917513
ju-ki <juki.programming@gmail.com>
2024-05-09 07:14:09 +0900

    tagとdifficultyを別のエンドポイントとして設定

--- server/src/meta-music/meta-music.controller.ts
+++ server/src/meta-music/meta-music.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { MetaMusicService } from './meta-music.service';
 
 @Controller('meta-music')
-export class MetaMusicController {}
+export class MetaMusicController {
+  constructor(private metaMusicService: MetaMusicService) {}
+
+  @Get('/difficulty')
+  findAllMusicDifficulty() {
+    return this.metaMusicService.fetchMusicDifficulties();
+  }
+  @Get('/tag')
+  findAllMusicTag() {
+    return this.metaMusicService.fetchMusicTag();
+  }
+}
