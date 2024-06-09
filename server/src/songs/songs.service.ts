63d218c786af9640244c30559215ee69d911377d
ju-ki <juki.programming@gmail.com>
2024-06-09 21:39:14 +0900

    #41 夢ステの楽曲情報取得のAPI作成

--- server/src/songs/songs.service.ts
+++ server/src/songs/songs.service.ts
@@ -1,15 +1,14 @@
-import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { ConfigService } from '@nestjs/config';
 import { MetaMusicService } from 'meta-music/meta-music.service';
 import { searchWords } from './dto';
 import axios from 'axios';
+import * as cheerio from 'cheerio';
 
 @Injectable({})
 export class SongsService {
   constructor(
-    private httpService: HttpService,
     private config: ConfigService,
     private prisma: PrismaService,
     private metaMusicService: MetaMusicService,
@@ -85,6 +84,101 @@ export class SongsService {
     }
   }
 
+  private headers = {
+    'User-Agent':
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+  };
+
+  async fetchYumesuteSongs() {
+    const loadUrl = this.config.get('YUMESUTE_URL');
+    const response = await axios.get(loadUrl, { headers: this.headers });
+    if (response.status === 200) {
+      const $ = cheerio.load(response.data);
+      const songItems = $('div.mu__table.mu__table--filter');
+      const songs = [];
+      const difficulty = ['normal', 'hard', 'extra', 'stella', 'olivier'];
+
+      //TODO:二週目行っている
+      const rows = songItems.find('table tbody tr').toArray();
+
+      for (const [idx, tr] of rows.entries()) {
+        await this.prisma.$transaction(async (prisma) => {
+          const td = $(tr).find('td');
+          const songUrl = td.eq(0).find('a').attr('href');
+
+          try {
+            const detailResponse = await axios.get(songUrl, { headers: this.headers });
+            if (detailResponse.status === 200) {
+              const detail$ = cheerio.load(detailResponse.data);
+              const songDetail = detail$('div.markup.mu').find('div.mu__table').eq(3);
+              const noteDetail = songDetail.find('tbody tr.mu__table--row3 td');
+              const noteCountList = [];
+
+              noteDetail.each((idx, detail) => {
+                const noteCount = $(detail).text() === '-' ? 0 : parseInt($(detail).text().replace(',', ''), 10);
+                noteCountList.push(noteCount);
+              });
+
+              const song = {
+                url: songUrl,
+                title: td.eq(1).find('a').text(),
+                note_count: noteCountList,
+                normal: td.eq(2).text(),
+                hard: td.eq(3).text(),
+                extra: td.eq(4).text(),
+                stella: td.eq(5).text(),
+                olivier: td.eq(6).text(),
+              };
+
+              await prisma.music.create({
+                data: {
+                  id: idx,
+                  name: td.eq(1).find('a').text(),
+                  assetBundleName: '',
+                  genreId: 2,
+                  releasedAt: new Date(),
+                },
+              });
+
+              for (const diff of difficulty) {
+                let totalNoteCount = song.note_count[difficulty.indexOf(diff)];
+                if (typeof totalNoteCount !== 'number') {
+                  totalNoteCount = parseInt(totalNoteCount);
+                }
+                await prisma.metaMusic.create({
+                  data: {
+                    id: `${idx}-${diff}`,
+                    genreId: 2,
+                    musicId: idx,
+                    playLevel: song[diff],
+                    totalNoteCount: totalNoteCount,
+                    musicDifficulty: diff,
+                  },
+                });
+              }
+
+              songs.push(song);
+            }
+          } catch (err) {
+            console.error('Error loading song detail:', err);
+          }
+
+          // 各ループの後にスリープ
+          await this.sleep(3000); // 2秒スリープ
+        });
+      }
+
+      return songs;
+    } else {
+      console.error(`Failed to retrieve the webpage. Status code: ${response.status}`);
+      return [];
+    }
+  }
+
+  private sleep(ms: number): Promise<void> {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
   async getDetailMusic(genreId, musicId) {
     const music = await this.prisma.music.findUnique({
       where: {
@@ -111,6 +205,10 @@ export class SongsService {
     if (typeof page !== 'number') {
       page = Number.parseInt(page);
     }
+    let genreId = query.genreId;
+    if (typeof genreId !== 'number') {
+      genreId = Number.parseInt(genreId);
+    }
 
     const isInfinityScroll = query.isInfinityScroll.toLowerCase() !== 'false';
 
@@ -127,6 +225,7 @@ export class SongsService {
         name: {
           contains: query.title,
         },
+        genreId: genreId,
         metaMusic: {
           some: {
             musicDifficulty: query.musicDifficulty,
@@ -165,11 +264,11 @@ export class SongsService {
             playLevel: query.playLevel,
           },
         },
-        musicTag: {
-          some: {
-            tagId: tagId,
-          },
-        },
+        // musicTag: {
+        //   some: {
+        //     tagId: tagId,
+        //   },
+        // },
       },
     });
 
