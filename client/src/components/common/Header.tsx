95a9d33a39a707adbc442d1636e3a0e5e720076a
ju-ki <juki.programming@gmail.com>
2024-06-27 22:00:46 +0900

    * DBからデータ取得する前に認証が必要な画面が表示されていたバグの修正 * リフレッシュトークンがうまく活用できていなかったため修正 * リフレッシュトークンをCookie上に保存するよう修正

--- client/src/components/common/Header.tsx
+++ client/src/components/common/Header.tsx
@@ -1,71 +1,25 @@
-import { CodeResponse, useGoogleLogin } from '@react-oauth/google';
-import axiosClient from '../../utils/axios';
+// import { CodeResponse, useGoogleLogin } from '@react-oauth/google';
+// import axiosClient from '../../utils/axios';
 import { Google } from '@mui/icons-material';
 import { Button, Typography } from '@mui/material';
 import { useUserStore } from '../store/userStore';
 import { useEffect } from 'react';
+import { useGoogleLogin } from '@react-oauth/google';
 
 const Header = () => {
-  const { user, isLoggedIn } = useUserStore();
+  const { user, isLoggedIn, logout, checkLoginStatus, handleGoogleLoginSuccess, handleGoogleLoginFailure } =
+    useUserStore();
 
   useEffect(() => {
     checkLoginStatus();
   }, []);
+
   const login = useGoogleLogin({
     onSuccess: (codeResponse) => handleGoogleLoginSuccess(codeResponse),
     flow: 'auth-code',
     scope: 'email profile openid',
     onError: (error) => handleGoogleLoginFailure(error),
   });
-
-  const handleGoogleLoginSuccess = async (
-    response: Omit<CodeResponse, 'error' | 'error_description' | 'error_uri'>,
-  ) => {
-    try {
-      const data = await axiosClient.post(`${import.meta.env.VITE_APP_URL}auth/google/login`, {
-        code: response.code,
-      });
-      useUserStore.getState().setAccessToken(data.data.accessToken);
-      useUserStore.getState().setUser(data.data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const handleGoogleLoginFailure = (error: Pick<CodeResponse, 'error' | 'error_description' | 'error_uri'>) => {
-    console.error('Google login error:', error);
-  };
-
-  const handleLogout = () => {
-    useUserStore.getState().logout();
-  };
-
-  async function checkLoginStatus() {
-    const accessToken = localStorage.getItem('accessToken');
-    if (!accessToken) {
-      console.log('No access token found');
-      return false;
-    }
-
-    const user = useUserStore.getState().user;
-
-    try {
-      const isValid = await axiosClient.get(`${import.meta.env.VITE_APP_URL}auth/verify`, {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-        params: {
-          userId: user?.id || null,
-        },
-      });
-      useUserStore.getState().setIsLoggedIn(isValid.data);
-
-      return true;
-    } catch (error) {
-      console.log('Token validation failed:', error);
-      return false;
-    }
-  }
 
   return (
     <div className=' text-white p-4 flex justify-between items-center'>
@@ -76,7 +30,7 @@ const Header = () => {
       {isLoggedIn ? (
         <div className='flex mx-4'>
           <img className='h-10 w-10 rounded-full mx-5' src={user?.imageUrl} alt='Profile' />
-          <Button variant='contained' onClick={handleLogout}>
+          <Button variant='contained' onClick={logout}>
             ログアウト
           </Button>
         </div>
