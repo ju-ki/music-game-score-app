a26cdc3fef24b1cda1c9684f3fe263c2c1cd613b
ju-ki <juki.programming@gmail.com>
2024-06-30 00:13:04 +0900

    originの追加

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,7 +7,11 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://music-game-score-app-frontend.onrender.com'], // フロントエンドのオリジンを指定
+    origin: [
+      'http://localhost:5173',
+      'https://music-game-score-app-frontend.onrender.com',
+      'https://music-game-score-app-management.onrender.com',
+    ], // フロントエンドのオリジンを指定
     allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization', // 'Authorization' ヘッダーを追加
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // 許可されたメソッド
     credentials: true,
