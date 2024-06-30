b8ce3079f405472257f71724f64f2b5fc31b1fd5
ju-ki <juki.programming@gmail.com>
2024-06-30 10:48:13 +0900

    オリジンを手動で設定

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,11 +6,16 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.enableCors({
-    origin: ['https://music-game-score-app-management.onrender.com'], // フロントエンドのオリジンを指定
-    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization', // 'Authorization' ヘッダーを追加
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // 許可されたメソッド
-    credentials: true,
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', process.env.TOP_PAGE);
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(200);
+    } else {
+      next();
+    }
   });
   app.use(cookieParser());
   await app.listen(3000);
