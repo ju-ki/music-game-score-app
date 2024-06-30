48ec0b3a2147cd52154810bcc7da71c29a70fe9e
ju-ki <juki.programming@gmail.com>
2024-06-30 21:03:03 +0900

    色々修正

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -5,12 +5,21 @@ const axiosClient = axios.create({
 });
 
 axiosClient.interceptors.request.use((request) => {
-  console.log('Request:', request);
+  console.log('Starting Request', {
+    url: request.url,
+    method: request.method,
+    headers: request.headers,
+    data: request.data,
+  });
   return request;
 });
 
 axiosClient.interceptors.response.use((response) => {
-  console.log('Response:', response);
+  console.log('Response:', {
+    status: response.status,
+    headers: response.headers,
+    data: response.data,
+  });
   return response;
 });
 
--- server/src/main.ts
+++ server/src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   });
 
   app.use(cookieParser());
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
